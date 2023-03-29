ea35e4ed3613ca13dee2629a489ba5ed9d36b90b
Yuri Maximo <yurisouza1712@gmail.com>
2023-03-29 08:14:13 -0300

    feature: adding s3 backend

--- provider.tf
+++ provider.tf
@@ -5,6 +5,11 @@ terraform {
       version = "4.48.0"
     }
   }
+  backend "s3" {
+    bucket = "commn-devops"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
