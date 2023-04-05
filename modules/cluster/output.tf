c921bdd25a497f0ba6834514f6902d444b957e25
Yuri Maximo <yurisouza1712@gmail.com>
2023-04-05 07:08:22 -0300

    feat: eks cluster creation and outputs/sg-rule

---
+++ modules/cluster/output.tf
@@ -0,0 +1,3 @@
+output "eks_vpc_config" {
+  value = aws_eks_cluster.eks_cluster.vpc_config
+}
--- modules/cluster/variables.tf
+++ modules/cluster/variables.tf
@@ -7,3 +7,13 @@ variable "tags" {
   type        = map(any)
   description = "Tags to be added to AWS resources"
 }
+
+variable "public_subnet_1a" {
+  type        = string
+  description = "Subnet to create EKS Cluster - AZ 1A"
+}
+
+variable "public_subnet_1b" {
+  type        = string
+  description = "Subnet to create EKS Cluster - AZ 1b"
+}
